bebbd5686bb8e0680a4f9bd0f6ecb02c0ffdde96
Matthew Malone <m.malone@me.com>
2019-07-27 15:00:52 -0600

    Functionality to add items from asied into the card

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var toDoArray = []
 // QUERY SELECTORS
 var aside = document.querySelector('.aside');
 var asideIdeas = document.querySelector('.form__div-idea-input');
+var main = document.querySelector('.main')
 
 
 // EVENT LISTENERS
@@ -17,23 +18,61 @@ aside.addEventListener('click', function () {
 })
 
 
+function addTitleToCard(title, taskItems) {
+  console.log('title: ', title)
+  main.insertAdjacentHTML(
+    'afterbegin',
+    `<article class="todo-card">
+      <h3 class="todo-card__h3" >${title.value}</h3>
+      <div class="todo-card__div-sperator1"></div>
+      ${taskItems}
+        <div class="todo-card__div-sperator2"></div>
+        <container class="todo-card-footer__container">
+          <div class="todo-card-footer__container__div">
+            <img class="todo-card-footer__container__div1__img urgent-image image" src="images/urgent.svg" alt="unactive image urgent status">
+              <p class="todo-card-footer__container__div urgent">URGENT</p>
+            </div>
+            <div class="todo-card-footer__container__div">
+              <img class="todo-card-footer__container__div2__img delete-image image" src="images/delete.svg" alt="unactive delete button">
+                <p class="todo-card-footer__container__div delete" >DELETE</p>
+            </div>
+          </container>
+        </article>`
+  )
+  clearInput(title)
+}
+
 function addInitialItems() {
   var asideIdeaInput = document.querySelector('.form__section-input')
   asideIdeas.insertAdjacentHTML(
     'beforeend',
     `<container class="form__container container1">
-      <input type="image" class="form__container-img image" src="images/delete.svg" alt="delete task item button">
-      <p class="form__container-p" >${asideIdeaInput.value}</p>
+    <input type="image" class="form__container-img image" src="images/delete.svg" alt="delete task item button">
+    <p class="form__container-p" >${asideIdeaInput.value}</p>
     </container>`
-  );
+    );
   var taskInput = document.querySelector('.form__section-input')
   clearInput(taskInput)
 }
 
 function makeTaskList(event) {
-  var itemsFromAside = getItemsFromAside(event)
-  
-  console.log(itemsFromAside)
+  var itemsFromAside = getItemsFromAside(event);
+  var asideTitleInput = document.querySelector('.form__input');
+  var asideIdeaList = document.querySelector('.form__div-idea-input')
+  var asideItemsInHtml = makeAsideItemHtml(itemsFromAside)
+  addTitleToCard(asideTitleInput, asideItemsInHtml);
+  asideIdeaList.innerHTML = '';
+}
+
+function makeAsideItemHtml(arrayOfItems) {
+  var createdHtmlArray = []
+  for (var i = 0; i < arrayOfItems.length; i++) {
+    createdHtmlArray.push(`<div class="todo-card-item__div">
+        <img class="todo-card-item__div__img image" src="images/checkbox.svg" alt="unchecked todo checkbox">
+          <p class="todo-card-item__div__p">${arrayOfItems[i]}</p>
+          </div>`)
+  }
+  return createdHtmlArray.join(' ')
 }
 
 function getItemsFromAside(event) {
@@ -44,6 +83,8 @@ function getItemsFromAside(event) {
   }
   return tempArray
 }
+
+
 
 function clearInput(input) {
   input.value = '';
