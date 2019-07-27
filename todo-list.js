49dfb36cf5dfb2d0e66dce7a245a48feec72dff9
Matthew Malone <m.malone@me.com>
2019-07-27 08:22:57 -0600

    ToDoList constructor and several methods

---
+++ todo-list.js
@@ -0,0 +1,24 @@
+class ToDoList {
+  constructor(id, title, urgent, tasks)
+    this.id = id,
+    this.title = title;
+    this.urgent = false;
+    this. tasks = []
+}
+
+// methods
+saveToStorage() {
+
+}
+
+deleteFromStorage() {
+
+}
+// updateToDo(should update the todo’s title and urgency)
+updateToDo() {
+
+}
+// updateTask(should update a task’s content and if it has been completed)
+updateTask() {
+
+}
