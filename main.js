6ae31e48dbd96e5a0b0776e89ce96bf445b706fb
Matthew Malone <m.malone@me.com>
2019-07-27 17:28:40 -0600

    Function to instantiate new class of ToDOList and save to global array

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+// Global Array
 var toDoArray = []
 
 // QUERY SELECTORS
 var aside = document.querySelector('.aside');
 var asideIdeas = document.querySelector('.form__div-idea-input');
+var asideTitleInput = document.querySelector('.form__input');
 var main = document.querySelector('.main')
 
 
@@ -13,53 +15,31 @@ aside.addEventListener('click', function () {
     addInitialItems();
   }
   if (event.target.closest('.make')) {
-    createTaskCard(event);
+    newToDo(event);
   }
 })
 
-
-function appendToDoCard(title, taskItems) {
-  console.log('title: ', title)
-  main.insertAdjacentHTML(
-    'afterbegin',
-    `<article class="todo-card">
-      <h3 class="todo-card__h3" >${title.value}</h3>
-      <div class="todo-card__div-sperator1"></div>
-      ${taskItems}
-        <div class="todo-card__div-sperator2"></div>
-        <container class="todo-card-footer__container">
-          <div class="todo-card-footer__container__div">
-            <img class="todo-card-footer__container__div1__img urgent-image image" src="images/urgent.svg" alt="unactive image urgent status">
-              <p class="todo-card-footer__container__div urgent">URGENT</p>
-            </div>
-            <div class="todo-card-footer__container__div">
-              <img class="todo-card-footer__container__div2__img delete-image image" src="images/delete.svg" alt="unactive delete button">
-                <p class="todo-card-footer__container__div delete" >DELETE</p>
-            </div>
-          </container>
-        </article>`
-  )
-  clearInput(title)
+function newToDo(event) {
+  var toDoObj = new ToDoList(Date.now(), asideTitleInput.value);
+  toDoArray.push(toDoObj);
+  // toDo.saveToStorage(toDoArray);
+  createTaskCard(event, toDoObj)
+  console.log('function newToDo', toDoObj)
 }
 
-function addInitialItems() {
-  var asideIdeaInput = document.querySelector('.form__section-input')
-  asideIdeas.insertAdjacentHTML(
-    'beforeend',
-    `<container class="form__container container1">
-    <input type="image" class="form__container-img image" src="images/delete.svg" alt="delete task item button">
-    <p class="form__container-p" >${asideIdeaInput.value}</p>
-    </container>`
-    );
-  var taskInput = document.querySelector('.form__section-input')
-  clearInput(taskInput)
-}
-
-function createTaskCard(event) {
-  var asideTitleInput = document.querySelector('.form__input');
+function createTaskCard(event, toDoObj) {
   var asideItemsInHtml = makeAsideItemHtml(event)
-  appendToDoCard(asideTitleInput, asideItemsInHtml);
+  appendToDoCard(toDoObj, asideItemsInHtml);
+  pushItemsIntoToDoObj(getItemsFromAside(event), toDoObj);
+  clearInput(asideTitleInput, toDoObj);
   document.querySelector('.form__div-idea-input').innerHTML = ''
+  console.log('function createTaskCard', toDoObj)
+}
+
+function pushItemsIntoToDoObj(arraOfItems, toDoObj) {
+  for (var i = 0; i < arraOfItems.length; i++) {
+    toDoObj.tasks.push(`{ check: false, item: ${arraOfItems[i]}}`)
+  }
 }
 
 function makeAsideItemHtml(event) {
@@ -81,6 +61,42 @@ function getItemsFromAside(event) {
     tempArray.push(items[i].innerText)
   }
   return tempArray
+}
+
+function appendToDoCard(toDoObj, taskItems) {
+  main.insertAdjacentHTML(
+    'afterbegin',
+    `<article class="todo-card" data-id="${toDoObj.id}">
+      <h3 class="todo-card__h3" >${toDoObj.title}</h3>
+      <div class="todo-card__div-sperator1"></div>
+      ${taskItems}
+      <div class="todo-card__div-sperator2"></div>
+      <container class="todo-card-footer__container">
+        <div class="todo-card-footer__container__div">
+          <img class="todo-card-footer__container__div1__img urgent-image image" src="images/urgent.svg" alt="unactive image urgent status">
+            <p class="todo-card-footer__container__div urgent">URGENT</p>
+        </div>
+        <div class="todo-card-footer__container__div">
+          <img class="todo-card-footer__container__div2__img delete-image image" src="images/delete.svg" alt="unactive delete button">
+          <p class="todo-card-footer__container__div delete" >DELETE</p>
+        </div>
+      </container>
+    </article>`
+  )
+
+}
+
+function addInitialItems() {
+  var asideIdeaInput = document.querySelector('.form__section-input')
+  asideIdeas.insertAdjacentHTML(
+    'beforeend',
+    `<container class="form__container container1">
+    <input type="image" class="form__container-img image" src="images/delete.svg" alt="delete task item button">
+    <p class="form__container-p" >${asideIdeaInput.value}</p>
+    </container>`
+    );
+  var taskInput = document.querySelector('.form__section-input')
+  clearInput(taskInput)
 }
 
 function clearInput(input) {
